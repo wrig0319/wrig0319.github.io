4ee31b1866eb7508d538a9a37f97341f14c3ccee
wrig0319 <46505607+wrig0319@users.noreply.github.com>
2019-04-22 23:42:57 -0400

    final

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,2 +1,19 @@
 window.onload = init;
 
+function init() {
+	document.querySelector('#color-1').onclick = changeColor;
+	document.querySelector('.reset').onclick = colorReset;
+}
+
+function changeColor() {
+	var elementId = this.id;
+
+	this.style.backgroundColor = "red";
+
+	var indicatorText = document.getElementById("indicator-text");
+
+	if(!indicatorText.innerHTML.includes(elementId)) {
+		indicatorText.innerHTML = indicatorText.innerHTML+"<br>"+this.id+ " is active";
+	}
+
+}
